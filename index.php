7df9b0a2b568603c03d58ea465d77ee2ee1737fb
mosesgerrard <mkadsamba86@gmail.com>
2015-09-05 22:02:28 -0430

    second commit - the changes on index file

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>GETTING STARTED WITH BRACKETS</title>
+        <title>GETTING STARTED WITH GIT</title>
         <meta name="description" content="An interactive getting started guide for Brackets.">
         <link rel="stylesheet" href="main.css">
     </head>
     <body>
         
-        <h1>GETTING STARTED WITH BRACKETS</h1>
+        <h1>GETTING STARTED WITH GIT</h1>
         <h2>This is your guide!</h2>
         
         <!--
